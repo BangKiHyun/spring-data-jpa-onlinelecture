58b54b3cc07588879781ee76bc63e57c10b6ea1b
BangKiHyun <rlrlvh@naver.com>
2020-09-22 03:19:58 +0900

    Update Spring Data Jpa 기반 레포지토리
    Spring Data Jpa 기반 레포지토리 CRUD 테스트
    메서드 쿼리 구현 및 테스트

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -14,37 +14,37 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @Transactional
 @Rollback(false)
-class MemberJpaRepositoryTest {
+class MemberRepositoryTest {
 
     @Autowired
-    MemberJpaRepository memberJpaRepository;
+    MemberRepository memberRepository;
 
     @Test
     public void basicCRUD() throws Exception {
         Member member1 = new Member("member1");
         Member member2 = new Member("member2");
-        memberJpaRepository.save(member1);
-        memberJpaRepository.save(member2);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
 
         //단건 조회 검증
-        Member findMember1 = memberJpaRepository.find(member1.getId());
-        Member findMember2 = memberJpaRepository.find(member2.getId());
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
         assertThat(findMember1).isEqualTo(member1);
         assertThat(findMember2).isEqualTo(member2);
 
         //리스트 조회 검증
-        final List<Member> all = memberJpaRepository.findAll();
+        final List<Member> all = memberRepository.findAll();
         assertThat(all.size()).isEqualTo(2);
 
         //카운트 검증
-        long count = memberJpaRepository.count();
+        long count = memberRepository.count();
         assertThat(count).isEqualTo(2);
 
         //삭제 검증
-        memberJpaRepository.delete(member1);
-        memberJpaRepository.delete(member2);
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
 
-        long deleteCount = memberJpaRepository.count();
+        long deleteCount = memberRepository.count();
         assertThat(deleteCount).isEqualTo(0);
     }
 
@@ -52,10 +52,10 @@ class MemberJpaRepositoryTest {
     void findByUsernameAndAgeGreaterThan() {
         Member m1 = new Member("AAA", 10);
         Member m2 = new Member("AAA", 20);
-        memberJpaRepository.save(m1);
-        memberJpaRepository.save(m2);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
 
-        final List<Member> result = memberJpaRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+        final List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
 
         assertThat(result.get(0).getUsername()).isEqualTo("AAA");
         assertThat(result.get(0).getAge()).isEqualTo(20);
